1e75cfe2a1c7a408877477739398bb5245a5365b
EC2 Default User <ec2-user@ip-172-31-47-82.ec2.internal>
2019-11-26 13:52:10 +0000

    updated validation for User and Film models

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,5 +1,10 @@
 class Film < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :title, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :director, presence: true, length: { maximum: 50 }
+  validates :image, presence: true
+  validates :lang, presence: true
+  validates :description, presence: true
   
   def self.search(search)
     if search
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
+  validates :image, presence: true
+  validates :password, confirmation: true
   mount_uploader :image, ImageUploader
   has_secure_password
   #has_secure_password is Rails' default function
