bcc17164bf18eb2c93b218263c332a500630c817
EC2 Default User <ec2-user@ip-172-31-47-82.ec2.internal>
2019-12-07 02:59:02 +0000

    added a validation for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   validates :image, presence: true
-  validates :password, confirmation: true
+  validates :password, presence: true, confirmation: true
   mount_uploader :image, ImageUploader
   has_secure_password
   #has_secure_password is Rails' default function
